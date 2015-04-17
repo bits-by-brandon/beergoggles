9d18138e6b27d9cfbda6ce2447f13422c25eaa83
Justin <jworkmandevelopment@gmail.com>
2015-04-17 10:52:41 -0400

    created clipped div for images to be inside of

--- main.js
+++ main.js
@@ -3,13 +3,14 @@
 //add div and goggle img to body tag of DOM
 document.body.innerHTML += '<svg version="1.1" id="gogglesMain" style="position:fixed;z-index:501" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="600px" height="280px" viewBox="0 0 317.361 139.413" enable-background="new 0 0 317.361 139.413" xml:space="preserve"> <g><path fill="#231F20" d="M247.654,7c34.577,0,62.707,28.13,62.707,62.707s-28.13,62.707-62.707,62.707 c-33.396,0-45.216-21.782-52.278-34.797l-0.533-0.982c-4.753-8.73-15.883-29.175-36.162-29.175 c-20.277,0-31.006,19.705-36.162,29.175l-0.534,0.983c-7.062,13.014-18.882,34.796-52.277,34.796C35.13,132.413,7,104.283,7,69.707 S35.13,7,69.707,7h78.683h20.582H247.654 M247.654,0c-13.343,0-49.901,0-78.683,0c-5.938,0-12.993,0-20.582,0 c-28.781,0-65.34,0-78.683,0C31.208,0,0,31.208,0,69.707c0,38.498,31.208,69.707,69.707,69.707s51.866-26.404,58.959-39.433 c5.621-10.324,14.696-25.522,30.014-25.522c15.182,0,24.394,15.198,30.014,25.522c7.094,13.028,20.462,39.433,58.96,39.433 c38.499,0,69.707-31.209,69.707-69.707C317.361,31.208,286.153,0,247.654,0L247.654,0z"/><clipPath id="svgPath"><path id="clipPath" fill="000" stroke="#231F20" stroke-miterlimit="10" d="M247.654,0c-13.343,0-49.901,0-78.683,0c-5.938,0-12.993,0-20.582,0 c-28.781,0-65.34,0-78.683,0C31.208,0,0,31.208,0,69.707c0,38.498,31.208,69.707,69.707,69.707s51.866-26.404,58.959-39.433 c5.621-10.324,14.696-25.522,30.014-25.522c15.182,0,24.394,15.198,30.014,25.522c7.094,13.028,20.462,39.433,58.96,39.433 c38.499,0,69.707-31.209,69.707-69.707C317.361,31.208,286.153,0,247.654,0L247.654,0z"/></clipPath></g></svg>';
 
-document.body.innerHTML += '<img id="background" style="position:fixed;width:100vw;height:100vh;top:0;left:0;z-index:500;-webkit-clip-path:url(#svgPath)"/>';
+document.body.innerHTML += '<div id="background" style="position:fixed;width:100vw;height:100vh;top:0;left:0;z-index:500;-webkit-clip-path:url(#svgPath)"><img id="overlay" style="position:fixed;width:100%;height:100%;top:0px;left:0px;z-index:10"></div>';
 
 var body = document.getElementsByTagName('body');
 var clipPath = document.getElementById('clipPath');
 var clip = document.getElementById('svgPath');
 var goggles = document.getElementById('gogglesMain');
 var background = document.getElementById('background');
+var overlay = document.getElementById('overlay');
 var image;
 
 //sends out a message to background.js that returns the Data URL to the screenshot
@@ -21,13 +22,13 @@ chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
 
 //applies the screenshot to #background src
 function setImage(image){
-   background.src = image;
+   overlay.src = image;
    detectFace();
 };
 
 //passes background image into facial detection
 function detectFace(){
-   $('#background').faceDetection({
+   $('#overlay').faceDetection({
         complete: function (faces) {
             for(i = 0; i < faces.length; i++){
                console.log(faces[i].x + " " + faces[i].y);
@@ -38,7 +39,7 @@ function detectFace(){
                var faceHeight = face.height;
                var faceWidth = face.width;
                
-               document.body.innerHTML += '<div class="face" style="position:fixed;background-color:#be67d8 ;height:'+ faceHeight +'px;width:'+ faceWidth +'px;z-index:400;left:' + faceX + 'px;top:'+ faceY +'px;">';
+               background.innerHTML += '<div class="face" style="position:fixed;background-color:#be67d8 ;height:'+ faceHeight +'px;width:'+ faceWidth +'px;z-index:400;left:' + faceX + 'px;top:'+ faceY +'px;">';
             }
         }
     });
